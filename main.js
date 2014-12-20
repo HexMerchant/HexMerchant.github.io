4fff3871429b6152adcce391f7db7d95b833e848
Bryan <HexMerchant@users.noreply.github.com>
2014-12-20 13:05:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,19 @@ var fire = 0;
 var explore = 0;
 var stick = 0;
 
+var learnAir = 0;
+var learnWater = 0;
+var learnEarth = 0;
+var learnFire = 0;
+
 function strengthClick(number){									//takes the number from the html code I think
 	strength = strength + number;								//this increases strength by 1 
 	document.getElementById("strength").innerHTML = strength;  //this updates the game for the user
+}
+
+function exploreClick(number){									//takes the number from the html code I think
+	explore = explore + number;								  //this increases strength by 1 
+	document.getElementById("explore").innerHTML = explore;  //this updates the game for the user
 }
 
 window.setInterval(function(){									// calls what is inside function every second
