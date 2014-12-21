e03acced08e89104ac3427907ef7267936bcac1a
Bryan <HexMerchant@users.noreply.github.com>
2014-12-20 22:39:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,20 +15,9 @@ var learnWater = 0;
 var learnEarth = 0;
 var learnFire = 0;
 
-function strengthClick(number){									//takes the number from the html code I think
-	strength = strength + number;								//this increases strength by 1 
-	document.getElementById("strength").innerHTML = strength;  //this updates the game for the user
-}
+function functionName() {
 
-function buyExplore(){
-    var exploreCost =  Math.floor(10 * Math.pow(1.0,explore)); 	 //works out the cost of this cursor
-    if(strength >= exploreCost){                                   //checks that the player can afford the cursor
-        explore = explore + 1;                                   //increases number of cursors
-    	strength = strength - exploreCost;                          //removes the cookies spent
-        document.getElementById('explore').innerHTML = explore;  //updates the number of cursors for the user
-        document.getElementById('strength').innerHTML = strength;  //updates the number of cookies for the user
-    };
-};
+}
 
 window.setInterval(function(){									// calls what is inside function every second
 
