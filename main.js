dde7089adfd130c652d328a58e62f4a63248a6be
Bryan <HexMerchant@users.noreply.github.com>
2014-12-21 07:29:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 var strength = 0;
 var intelligence = 0;
-var dexterity = 0;
+var defence = 0;
 var speed = 0;
-var stealth = 0;
 var earth = 0;
 var air = 0;
 var water = 0;
 var fire = 0;
-var explore = 0;
 var stick = 0;
+var rock = 0;
 
 var learnAir = 0;
 var learnWater = 0;
 var learnEarth = 0;
 var learnFire = 0;
 
-function functionName() {
+var current = 0;
+var person;
+var story = ["-You wake up lying on the ground. You feel a large deposit of 
+    energy inside you.", "-You go to the right.", "-The path is blocked."];
 
+function exploreFunction() {
+	if (current == 0) {
+		person = prompt("What is your name", "");
+	}
+	if (person != null) {
+		document.getElementById("story1").innerHTML = story[current];
+		current += 1;
+	}
 }
 
 window.setInterval(function(){									// calls what is inside function every second
