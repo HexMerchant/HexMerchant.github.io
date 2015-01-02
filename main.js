54c1162fcc03024ce7196f15e595bc22777aa355
Bryan <HexMerchant@users.noreply.github.com>
2015-01-01 17:34:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ Add a single stat "how many sticks have you gathered"
 var stick = 0;
 var rock = 0;
 var wood = 0;
+var bluntObjectValue = 0;
+
 var health = 50;
 var strength = 0;
 var intelligence = 0;
@@ -32,11 +34,14 @@ var story = ["-You wake up lying on the ground. You feel a large deposit \
 of energy inside you.", "-You look around only to see a thicket of trees.", "-You begin to walk around looking for anything that may help."];
 
 var stamina = 100;
+var flux = 0;
 
 var crafting1Name = "Craft Wood";
 var crafting1Cost = 10;
+var crafting2Cost = 13;
 
 var research1Progress = 0;
+
 
 function saveGame() {
 var save = {
@@ -44,9 +49,9 @@ stick: stick,
 rock: rock,
 wood: wood,
 stamina: stamina,
-current: current,
-person: person,
-story: story
+flux: flux,
+bluntObjectValue: bluntObjectValue,
+research1Progress: research1Progress
 	}
 localStorage.setItem("save",JSON.stringify(save));
 }
@@ -57,10 +62,15 @@ if (typeof savegame.stick !== "undefined") stick = savegame.stick;
 if (typeof savegame.rock !== "undefined") rock = savegame.rock;
 if (typeof savegame.wood !== "undefined") wood = savegame.wood;
 if (typeof savegame.stamina !== "undefined") stamina = savegame.stamina;
+if (typeof savegame.flux !== "undefined") flux = savegame.flux;
+if (typeof savegame.bluntObjectValue !== "undefined") bluntObjectValue = savegame.bluntObjectValue;
+if (typeof savegame.research1Progress !== "undefined") research1Progress = savegame.research1Progress;
 document.getElementById("stick").innerHTML = stick;
 document.getElementById("rock").innerHTML = rock;
 document.getElementById("wood").innerHTML = wood;
 document.getElementById("stamina").innerHTML = stamina;
+document.getElementById("flux").innerHTML = flux;
+document.getElementById("bluntObjectValue").innerHTML = bluntObjectValue;
 }
 
 function deleteSave() {
@@ -109,33 +119,65 @@ function craftWood() {
 	if (stick >= crafting1Cost) {
 	stick = stick - crafting1Cost;
 	wood = wood + 1;
-	document.getElementById("stick").innerHTML = stick;
-	document.getElementById("wood").innerHTML = wood;
+		document.getElementById("stick").innerHTML = stick;
+		document.getElementById("wood").innerHTML = wood;
+	}
+}
+
+function craftBluntObject() {
+	if (stick >= crafting2Cost) {
+	stick = stick - crafting2Cost;
+	bluntObjectValue = bluntObjectValue + 1;
+		document.getElementById("stick").innerHTML = stick;
+		document.getElementById("bluntObjectValue").innerHTML = bluntObjectValue;
 	}
 }
 
 function tick() {
- if (stamina < 100)
-	stamina = stamina + 1;
- document.getElementById("stamina").innerHTML = stamina;
+	if (stamina < 100)
+		stamina = stamina + 1;
+	document.getElementById("stamina").innerHTML = stamina;
+}
+
+function tickTwo() {
+	if (flux >= 1)
+		flux = flux - 1;
+		document.getElementById("flux").innerHTML = flux;
 }
 
 //Hide and Show Divs
-
+function hideContent(d) {
+document.getElementById(d).style.display = "none";
+}
+function showInlineContent(d) {
+document.getElementById(d).style.display = "inline";
+}
+function showBlockContent(d) {
+document.getElementById(d).style.display = "block";
+}
+function reverseDisplay(d) {
+if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
+else { document.getElementById(d).style.display = "none"; }
+}
 //Hide and Show Divs
 
+// Show Div <a href="javascript:showInlineContent('crafting2')">Click to show.</a>
 function researchStick() {
-	if (stick > 1 && research1Progress < 100)
+	if (stick >= 1 && research1Progress < 100) {			// Want to be able to show progress
 	stick = stick - 1;										// each time you click me I want to use 1 stick
 	research1Progress = research1Progress + 5;				// each stick brings research progress up 5
-	document.getElementById("stick").innerHTML = stick;
-	
-//Want to be able to show progress
-// when research hits 100 unlocks blunt object for crafting
-// this clickable button should hide after completion
-// add achievement for first research project
+	document.getElementById("stick").innerHTML = stick;		// Show the player the update
+	}
+	if (research1Progress == 100) {							// When reaches 100
+	showInlineContent('crafting2');							// show crafting2
+	showInlineContent('bluntObjectName');
+	showInlineContent('bluntObjectValue');
+	}
+	// when research hits 100 unlocks blunt object for crafting
+	// this clickable button should hide after completion
+	// add achievement for first research project
+
 }
-		
 
 window.setInterval(function() {
 
